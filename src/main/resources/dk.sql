a01dd1709da85b8a992d7f934fee54c9d9354fd8
daikai <13621151569@yeah.net>
2018-04-09 10:09:19 +0800

    部分未改的马甲

--- src/main/resources/dk.sql
+++ src/main/resources/dk.sql
@@ -4,7 +4,7 @@ Navicat MySQL Data Transfer
 Source Server         : 本地
 Source Server Version : 50610
 Source Host           : localhost:3306
-Source Database       : z77z
+Source Database       : dk
 
 Target Server Type    : MYSQL
 Target Server Version : 50610
